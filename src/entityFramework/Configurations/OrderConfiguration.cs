d6c1cebb3a029ab13df7ef27ea7586afd32cf8cc
AbdulhadiSB <hadibasulaiman@gmail.com>
2024-05-06 02:20:23 +0300

    created constraints for order

--- src/entityFramework/Configurations/OrderConfiguration.cs
+++ src/entityFramework/Configurations/OrderConfiguration.cs
@@ -1,27 +1,62 @@
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.EntityFrameworkCore.Metadata.Builders;
-// //using Store.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.entityFramework;
+using Store.EntityFramework.Entities;
 // using Store.Models;
 
-// namespace Store.EntityFramework.Configurations;
+namespace Store.EntityFramework.Configurations;
 
-// public class OrderConfiguration : IEntityTypeConfiguration<Order>
-// {
-//     public void Configure(EntityTypeBuilder<Order> builder)
-//     {
-//         ///#####################
-//         //TableBuilder
-//         //######################
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        ///#####################
+        //TableBuilder
+        //######################
+        builder.ToTable("Order");
 
-//         //builder.ToTable("TableName");
-//         //builder.Property(c => c.ClomenName).
+        builder.HasKey(o => o.OrderId);
 
-//         ///#####################
-//         //Table Relation
-//         ///#####################
+        builder.Property(u => u.OrderId)
+        .ValueGeneratedOnAdd();
 
-//         //builder.HasMany(c => c.ClomenName)
-//         // .WithOne(o => o.ClomenName)
-//         // .HasForeignKey(o => o.ClomenName);
-//     }
-// }
+        builder.Property(u => u.UserId)
+        .IsRequired();
+
+        builder.Property(ad => ad.AddressId)
+        .IsRequired();
+
+        builder.Property(pm => pm.PaymentMethodId)
+        .IsRequired();
+
+        builder.Property(t => t.TransactionId)
+        .IsRequired()
+        .ValueGeneratedOnAdd();
+
+        builder.Property(s => s.ShipmentId)
+        .IsRequired()
+        .ValueGeneratedOnAdd();
+
+        builder.Property(o => o.Status)
+        .IsRequired();
+
+        builder.Property(o => o.CreatedAt)
+        .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+
+
+
+
+
+        //builder.ToTable("TableName");
+        //builder.Property(c => c.ClomenName).
+
+        ///#####################
+        //Table Relation
+        ///#####################
+
+        //builder.HasMany(c => c.ClomenName)
+        // .WithOne(o => o.ClomenName)
+        // .HasForeignKey(o => o.ClomenName);
+    }
+}
