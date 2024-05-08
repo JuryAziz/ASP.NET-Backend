4adc845b36643ee09d23aa77a564634f87fb7224
ibrahemDev <iibrahemdev@gmail.com>
2024-05-08 07:23:20 +0000

    cleen code

--- src/entityFramework/Configurations/CategoryConfiguration.cs
+++ src/entityFramework/Configurations/CategoryConfiguration.cs
@@ -1,36 +1,6 @@
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.EntityFrameworkCore.Metadata.Builders;
-// //using Store.EntityFramework.Entities;
-// using Store.Models;
-
-// namespace Store.EntityFramework.Configurations;
-
-// public class CategoryConfiguration : IEntityTypeConfiguration<Category>
-// {
-//     public void Configure(EntityTypeBuilder<Category> builder)
-//     {
-//         ///#####################
-//         //TableBuilder
-//         //######################
-
-//         //builder.ToTable("TableName");
-//         //builder.Property(c => c.ClomenName).
-
-//         ///#####################
-//         //Table Relation
-//         ///#####################
-
-//         //builder.HasMany(c => c.ClomenName)
-//         // .WithOne(o => o.ClomenName)
-//         // .HasForeignKey(o => o.ClomenName);
-//     }
-// }
-
-
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Store.EntityFramework.Entities;
-
 
 namespace Store.EntityFramework.Configurations;
 
@@ -38,16 +8,12 @@ public class CategoryConfiguration : IEntityTypeConfiguration<Category>
 {
     public void Configure(EntityTypeBuilder<Category> builder)
     {
-
         builder.ToTable("category");
 
         builder.HasKey(c => c.CategoryId);
         builder.Property(c => c.CategoryId).HasColumnName("category_id").IsRequired().ValueGeneratedOnAdd().HasDefaultValueSql("gen_random_uuid()");
         builder.Property(c => c.Name).HasColumnName("name").IsRequired();
         builder.Property(c => c.Description).HasColumnName("description");
-
-
-
 
         builder.HasIndex(u => u.CategoryId).IsUnique();
         builder.HasIndex(u => u.Name).IsUnique();
@@ -63,20 +29,12 @@ public class CategoryConfiguration : IEntityTypeConfiguration<Category>
             .HasMany(category => category.ProductList)
             .WithMany(product => product.CategoryList)
             .UsingEntity<ProductCategory>(
-
                 l => l.HasOne(productCategory => productCategory.Product)
                     .WithMany(product => product.ProductCategoryList)
                     .HasForeignKey(productCategory => productCategory.ProductId).IsRequired(),
-
-
                 r => r.HasOne(productCategory => productCategory.Category)
                     .WithMany(category => category.ProductCategoryList)
                     .HasForeignKey(productCategory => productCategory.CategoryId).IsRequired()
-
             );
-
-
-
     }
-
 }
--- src/entityFramework/Configurations/ProductConfiguration.cs
+++ src/entityFramework/Configurations/ProductConfiguration.cs
@@ -2,17 +2,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Store.EntityFramework.Entities;
 
-
 namespace Store.EntityFramework.Configurations;
 
 public class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
     public void Configure(EntityTypeBuilder<Product> builder)
     {
-
         builder.ToTable("product");
         builder.HasKey(c => c.ProductId);
-
 
         builder.Property(c => c.ProductId).HasColumnName("product_id").IsRequired().ValueGeneratedOnAdd().HasDefaultValueSql("gen_random_uuid()");
         builder.Property(c => c.Name).HasColumnName("name").IsRequired();
@@ -20,11 +17,7 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
         builder.Property(c => c.Price).HasColumnName("price").IsRequired();
         builder.Property(c => c.Stock).HasColumnName("total_quantity").IsRequired().HasDefaultValue(0).ValueGeneratedOnAdd();
 
-
-
-
         builder.HasIndex(u => u.ProductId).IsUnique();
-
 
         // Product Has many ProductCategory
         builder
@@ -38,51 +31,9 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
             .HasMany(product => product.CategoryList)
             .WithMany(category => category.ProductList)
             .UsingEntity<ProductCategory>(
-                 l => l.HasOne(productCategory => productCategory.Category).WithMany(category => category.ProductCategoryList).HasForeignKey(productCategory => productCategory.CategoryId),
-
+                l => l.HasOne(productCategory => productCategory.Category).WithMany(category => category.ProductCategoryList).HasForeignKey(productCategory => productCategory.CategoryId),
                 r => r.HasOne(productCategory => productCategory.Product).WithMany(product => product.ProductCategoryList).HasForeignKey(productCategory => productCategory.ProductId)
+        );
 
-                );
-
-
-
-        /*builder
-        .HasMany(product => product.CategoryList)
-        .WithMany(category => category.ProductList)
-        .UsingEntity(
-            "product_category",
-            l => l.HasOne(typeof(Product)).WithMany().HasForeignKey("product_id"),
-            r => r.HasOne(typeof(Category)).WithMany().HasForeignKey("category_id"));*/
-
-
-
-
-        /**
-         builder
-            .HasMany(product => product.UserEntityListByReview)
-            .WithMany(user => user.ProductEntityListByReview)
-            .UsingEntity<ProductReviewEntity>(
-                 l => l.HasOne(productReview => productReview.UserEntity).WithMany(user => user.ProductReviewEntityList).HasForeignKey(productReview => productReview.UserId),
-
-                r => r.HasOne(productReview => productReview.ProductEntity).WithMany(product => product.ProductReviewEntityList).HasForeignKey(productReview => productReview.ProductId)
-
-                );
-        
-        */
-
-        ///#####################
-        //TableBuilder
-        //######################
-
-        //builder.ToTable("TableName");
-        //builder.Property(c => c.ClomenName).
-
-        ///#####################
-        //Table Relation
-        ///#####################
-
-        //builder.HasMany(c => c.ClomenName)
-        // .WithOne(o => o.ClomenName)
-        // .HasForeignKey(o => o.ClomenName);
     }
 }
