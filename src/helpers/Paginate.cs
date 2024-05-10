1d3eb0f1a6eba57c6ebc40294369c22eaae329bc
ItsAmmarB <13232108+ItsAmmarB@users.noreply.github.com>
2024-05-10 22:22:10 +0300

    increased max page limit to 50 items per page

--- src/helpers/Paginate.cs
+++ src/helpers/Paginate.cs
@@ -1,9 +1,9 @@
 namespace Store.Helpers;
 
 public static class Paginate {
-    public static List<T> Function<T>(List<T> itemsList, int page = 1, int limit = 25) {
+    public static List<T> Function<T>(List<T> itemsList, int page = 1, int limit = 50) {
         
-        if(limit > 25) limit = 25;
+        if(limit > 50) limit = 50;
         if(itemsList.Count < ((page - 1) * limit)) page = 1;
         if(itemsList.Count < (page  * limit)) {
             limit = itemsList.Count;
