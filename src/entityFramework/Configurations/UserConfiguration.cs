c78467d1b46ddaed49f79b28635421b99a3f59ae
ItsAmmarB <13232108+ItsAmmarB@users.noreply.github.com>
2024-05-10 22:25:08 +0300

    added All user relations needed

--- src/entityFramework/Configurations/UserConfiguration.cs
+++ src/entityFramework/Configurations/UserConfiguration.cs
@@ -15,30 +15,65 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.HasKey(u => u.UserId);
 
         builder
-        .Property(u => u.UserId)
-        .IsRequired()
-        .ValueGeneratedOnAdd();
+            .Property(u => u.UserId)
+            .IsRequired()
+            .ValueGeneratedOnAdd()
+            .HasDefaultValueSql("gen_random_uuid()");
 
         builder
-        .HasIndex(u => u.Email)
-        .IsUnique();
+            .HasIndex(u => u.Email)
+            .IsUnique();
 
         builder
-        .HasIndex(u => u.PhoneNumber)
-        .IsUnique();
+            .HasIndex(u => u.PhoneNumber)
+            .IsUnique();
 
         builder
-        .Property(u => u.FirstName)
-        .IsRequired();
+            .Property(u => u.FirstName)
+            .IsRequired();
+
+        builder
+            .Property(u => u.CreatedAt)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
 
         //###########################
         //      TableBuilder
         //###########################
 
-        // 1:N with Address
+        // Address Relation 1:N
         builder
-        .HasMany(u => u.Addresses)
-        .WithOne(a => a.User)
-        .HasForeignKey(a => a.UserId);
+            .HasMany(u => u.Addresses)
+            .WithOne(a => a.User)
+            .HasForeignKey(a => a.UserId);
+
+        // Payment Methods Relation 1:N
+        builder
+            .HasMany(u => u.PaymentMethods)
+            .WithOne(pm => pm.User)
+            .HasForeignKey(a => a.UserId);
+
+        // Orders Relation 1:N
+        builder
+            .HasMany(u => u.Orders)
+            .WithOne(o => o.User)
+            .HasForeignKey(o => o.UserId);
+
+        // Shopping Lists Relation 1:N
+        // builder
+        //     .HasOne(u => u.ShoppingLists)
+        //     .WithOne(c => c.User)
+        //     .HasForeignKey(c => c.UserId);
+
+        // Cart Relation 1:1
+        builder
+            .HasOne(u => u.Cart)
+            .WithOne(c => c.User)
+            .HasForeignKey<Cart>(c => c.UserId);
+
+        // Product Reviews Relation 1:N
+        builder
+            .HasMany(u => u.ProductReviews)
+            .WithOne(pr => pr.User)
+            .HasForeignKey(pr => pr.UserId);
     }
 }
