85171ae66ae457c12c213be72ebac78a3bb4bc76
ItsAmmarB <13232108+ItsAmmarB@users.noreply.github.com>
2024-05-10 17:10:24 +0300

    reworked PaymentMethod Service for consistency

--- src/services/PaymentMethodService.cs
+++ src/services/PaymentMethodService.cs
@@ -6,32 +6,16 @@ using Store.Models;
 
 namespace Store.Application.Services;
 
-public class PaymentMethodService
+public class PaymentMethodService(AppDbContext appDbContext)
 {
-    private readonly AppDbContext _appDbContext;
+    private readonly AppDbContext _appDbContext = appDbContext;
 
-    public PaymentMethodService(AppDbContext appDbContext)
+    public async Task<List<PaymentMethod>> GetPaymentMethods()
     {
-        _appDbContext = appDbContext;
-    }
-
-    public async Task<IEnumerable<PaymentMethod>> GetPaymentMethods(int page, int limit)
-    {
-
-        var paymentMethods = _appDbContext.PaymentMethods.ToList();
-        var pagedPayment = paymentMethods[((page - 1) * limit)..(_appDbContext.PaymentMethods.Count() > (page * limit) ? page * limit : _appDbContext.PaymentMethods.Count())];
-        return await Task.FromResult(pagedPayment.AsEnumerable());
+        return await _appDbContext.PaymentMethods
+            .ToListAsync();
 
     }
-
-    // public async Task<IEnumerable<PaymentMethod>> GetUserPaymentMethods(Guid userId)
-    // {
-    //     return await Task.FromResult(
-    //         (await _appDbContext.PaymentMethods.ToListAsync())
-    //         .FindAll(pm => pm.UserId == userId)
-    //         );
-
-    // }
 
     public async Task<PaymentMethod?> GetPaymentMethodById(Guid paymentMethodId)
     {
@@ -44,7 +28,6 @@ public class PaymentMethodService
     {
         var paymentMethod = new PaymentMethod
         {
-            PaymentMethodId = Guid.NewGuid(),
             UserId = newPaymentMethod.UserId,
             Type = newPaymentMethod.Type,
             CardNumber = newPaymentMethod.CardNumber,
@@ -58,13 +41,11 @@ public class PaymentMethodService
         await _appDbContext.PaymentMethods.AddAsync(paymentMethod);
         await _appDbContext.SaveChangesAsync();
         return await Task.FromResult(paymentMethod);
-
     }
 
     public async Task<PaymentMethod?> UpdatePaymentMethod(Guid paymentMethodId, PaymentMethodModel updatedPaymentMethod)
     {
-        var paymentMethodToUpdate = await _appDbContext.PaymentMethods
-        .FirstOrDefaultAsync(pm => pm.PaymentMethodId == paymentMethodId);
+        var paymentMethodToUpdate = await GetPaymentMethodById(paymentMethodId);
         if (paymentMethodToUpdate is not null)
         {
             paymentMethodToUpdate.Type = updatedPaymentMethod.Type;
@@ -82,14 +63,13 @@ public class PaymentMethodService
 
     public async Task<bool> DeletePaymentMethod(Guid paymentMethodId)
     {
-        var paymentMethodToDelete = await _appDbContext.PaymentMethods
-        .FirstOrDefaultAsync(pm => pm.PaymentMethodId == paymentMethodId);
-        if (paymentMethodToDelete is not null)
-        {
-            _appDbContext.PaymentMethods.Remove(paymentMethodToDelete);
-            await _appDbContext.SaveChangesAsync();
-            return await Task.FromResult(true);
-        };
-        return await Task.FromResult(false);
+        var paymentMethodToDelete = await GetPaymentMethodById(paymentMethodId);
+        if (paymentMethodToDelete is not null) return await Task.FromResult(false);
+
+        _appDbContext.PaymentMethods.Remove(paymentMethodToDelete);
+        await _appDbContext.SaveChangesAsync();
+        
+        return await Task.FromResult(true);
+        
     }
 }
