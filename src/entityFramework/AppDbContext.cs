7a88d748fd537518f0ae84776e1880f7a7cc8553
Jury Alharbi <jury.abdulaziz@gmail.com>
2024-05-12 02:48:20 +0300

    Context has all

--- src/entityFramework/AppDbContext.cs
+++ src/entityFramework/AppDbContext.cs
@@ -5,17 +5,18 @@ namespace Store.EntityFramework;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions options)
-        : base(options) { }
-
-    // public DbSet<User> Users { get; set; }
-    // public DbSet<Profile> Profiles { get; set; }
-    // public DbSet<Order> Orders { get; set; }    // public DbSet<Profile> Profiles { get; set; }
-     public DbSet<Cart> Carts { get; set; }
-    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<PaymentMethod> PaymentMethods { get; set; }
     public DbSet<ShoppingList> ShoppingLists { get; set; }
-
-
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products{ get; set; }
+    public DbSet<ProductReview> ProductReviews { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    
     // use Fluent API
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
